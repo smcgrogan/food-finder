f566360fc5515d2b91799b88e35bc89519267e62
Aidan Srouji <72050533+asrouji@users.noreply.github.com>
2021-03-02 15:06:42 -0800

    Add Form component to ListItem.jsx

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import {Form, Button, Card} from "react-bootstrap";
 
 export default function ListItem() {
-  return <h1>ListItem Placeholder</h1>;
+  const [name, setName] = useState('');
+  const [desc, setDesc] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Control
+          type="input"
+          placeholder="Name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={3}
+          value={desc}
+          onChange={(event) => setDesc(event.target.value)}
+        />
+      </Form.Group>
+    </Form>
+  );
 }
