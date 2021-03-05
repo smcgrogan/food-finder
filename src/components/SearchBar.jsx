38b9fce51d27c3324661317ce98dfe66c77c13d2
smcgrogan <58006153+smcgrogan@users.noreply.github.com>
2021-03-04 16:30:19 -0800

    Updated SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,25 @@
-import { React } from "react";
+import React, { useEffect, useState } from "react";
+import FormControl from 'react-bootstrap/Form';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+
+
 export default function SearchBar(props) {
   const { title } = props;
-  return <h1> THIS is {title} SEARCH BAR WOHOOO</h1>;
+  return(
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Food Cuisine
+      </Dropdown.Toggle>
+  
+      <Dropdown.Menu>
+        <Dropdown.Item eventKey="1">American</Dropdown.Item>
+        <Dropdown.Item eventKey="2">Japanese</Dropdown.Item>
+        <Dropdown.Item eventKey="3">Italian</Dropdown.Item>
+        <Dropdown.Item eventKey="4">Chinese</Dropdown.Item>
+        <Dropdown.Item eventKey="5" active>Greek</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
 }
